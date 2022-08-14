60f62f42d3b2a613023a6850adb4a2e326d15279
Leeap83 <leeap83@gmail.com>
2022-08-14 14:42:50 +0000

    added product filter by price

--- products.php
+++ products.php
@@ -7,57 +7,72 @@ include('includes/dbh.inc.php');
     <div class="wrapper-main">
         <div class="product-section">
             <h1>All Products</h1>
-            <form action="products.php" method="POST">            
+            <form action="products.php" method="POST" class="search-form">            
                 <input type="text"  class="search-field" name="search" placeholder="Search here...">
                 <div class="search_box">
                     <input type="submit" class="btn back_btn submit">
                     <input type="submit" class="btn back_btn reset" value="All Products">
                 </div>
             </form>
-                <div class="product_grid">
-                    <?php
-                    if (isset($_POST['search'])){
-                        $search = $_POST['search'];
-                        $sql = "SELECT * FROM products WHERE product_name like '%$search%' or product_brand like '%$search%' or category like '%$search%'";
-                    } else {
-                        $sql = "SELECT * FROM products";
+
+            <div class="price-filter">
+                <p><strong>Price:</strong>
+                    <a href="products.php?sortby=pricelohi">Low - Hi</a>/ 
+                    <a href="products.php?sortby=pricehilo">Hi - Low</a>
+                </p>
+            </div>
+            <div class="product_grid">
+                <?php
+                if (isset($_POST['search'])){
+                    $search = $_POST['search'];
+                    $sql = "SELECT * FROM products WHERE product_name like '%$search%' or product_brand like '%$search%' or category like '%$search%'";
+                } elseif (isset($_GET['sortby'])) {
+                    $sortby = $_GET['sortby'];
+                    if ($sortby == 'pricehilo') {
+                        $sql = "SELECT * FROM products ORDER BY price DESC";
+                    } elseif ($sortby = 'pricelohi') {
+                        $sql = "SELECT * FROM products ORDER BY price ASC";
                     }
-                    $result = mysqli_query($conn, $sql);
-                    $resultCheck = mysqli_num_rows($result);
-                    if ($resultCheck > 0) {    
-                        while ($row = mysqli_fetch_assoc($result)) {?>
-                            <a href="product-details.php?id=<?= $row['id'] ?>">
-                                <div class="product_item card">
-                                    <?php
-                                    if ($row['in_stock'] == '0') {?>                                
-                                        <div class="img_container">
-                                            <span>SOLD</span> 
-                                            <img src="<?= $row['main_image'] ?>" width="300" height="300" alt="<?= $row['product_name'] ?>">
-                                        </div>
-                                    <?php
-                                    } elseif ($row['in_stock'] == '2') {?>
-                                        <div class="img_container">
-                                            <span>COMING SOON</span> 
-                                            <img src="<?= $row['main_image'] ?>" width="300" height="300" alt="<?= $row['product_name'] ?>">
-                                        </div>
-                                        <?php
-                                    } else {?>
+                } else {
+                    $sql = "SELECT * FROM products";
+                }
+                $result = mysqli_query($conn, $sql);
+                $resultCheck = mysqli_num_rows($result);
+                if ($resultCheck > 0) {    
+                    while ($row = mysqli_fetch_assoc($result)) {?>
+                        <a href="product-details.php?id=<?= $row['id'] ?>">
+                            <div class="product_item card">
+                                <?php
+                                if ($row['in_stock'] == '0') {?>                                
+                                    <div class="img_container">
+                                        <span>SOLD</span> 
                                         <img src="<?= $row['main_image'] ?>" width="300" height="300" alt="<?= $row['product_name'] ?>">
-                                    <?php
-                                    }
-                                    ?>
-                                    <div class="card_container">
-                                        <h2><?= $row['product_name'] ?></h2>
-                                        <p> £<?= number_format($row['price'])?></p> 
                                     </div>
+                                <?php
+                                } elseif ($row['in_stock'] == '2') {?>
+                                    <div class="img_container">
+                                        <span>COMING SOON</span> 
+                                        <img src="<?= $row['main_image'] ?>" width="300" height="300" alt="<?= $row['product_name'] ?>">
+                                    </div>
+                                    <?php
+                                } else {?>
+                                    <img src="<?= $row['main_image'] ?>" width="300" height="300" alt="<?= $row['product_name'] ?>">
+                                <?php
+                                }
+                                ?>
+                                <div class="card_container">
+                                    <h2><?= $row['product_name'] ?></h2>
+                                    <p> £<?= number_format($row['price'])?></p> 
+                                    <p><small>Year: <?= ($row['years'])?> | Hours: <?=($row['hours'])?></small></p>
                                 </div>
-                            </a>
-                            <?php
-                        }
-                    } else {
-                        echo '<p>No Products Found</p>';
-                    }?>
-                </div>
+                            </div>
+                        </a>
+                        <?php
+                    }
+                } else {
+                    echo '<p>No Products Found</p>';
+                }?>
+            </div>
         </div>
     </div>
 </main>
